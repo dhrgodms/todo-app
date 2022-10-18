2535071a775236650dd3bdeffa6235402982e0b9
dhrgodms <dhrgodms123@gmail.com>
2022-10-19 02:25:10 +0900

    change todo-title-bg-color

--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -4,7 +4,7 @@ export function TodoTemplate({children}){
     return (
       <div className="TodoTemplate">
         <div className="app-title">
-          🦖뽀짝뽀짝 해은이의 오늘 할 일🦖
+          🦖 뽀짝뽀짝 해은이의 오늘 할 일 🦖
         </div>
         <div className="content">{children}</div>
       </div>
